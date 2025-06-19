98327b4e5531ad7feb2802790aff314886c711e3
Gagonlaire <paulgervais911@gmail.com>
2025-06-20 01:18:28 +0200

    feat: add new protocol types

--- internal/mc/fields.go
+++ internal/mc/fields.go
@@ -1,206 +1,15 @@
 package mc
 
 import (
+	"encoding/binary"
 	"fmt"
 	"io"
+	"math"
 	"reflect"
 )
 
-// ReadFrom reads a Long from the provided io.Reader.
-//
-// Encodes:
-//   - An integer between -9223372036854775808 and 9223372036854775807.
-//
-// Notes:
-//   - Signed 64-bit integer, two's complement.
-func (l *Long) ReadFrom(r io.Reader) (n int64, err error) {
-	var buf [8]byte
-
-	if _, err = io.ReadFull(r, buf[:]); err != nil {
-		return 0, err
-	}
-	*l = 0
-	for i := 0; i < 8; i++ {
-		*l = (*l << 8) | Long(buf[i])
-	}
-
-	return 8, nil
-}
-
-// WriteTo writes a Long to the provided io.Writer.
-func (l Long) WriteTo(w io.Writer) (n int64, err error) {
-	var buf [8]byte
-
-	b := buf[:]
-	for i := 7; i >= 0; i-- {
-		b[i] = byte(l)
-		l >>= 8
-	}
-	_, err = w.Write(b)
-	if err != nil {
-		return 0, err
-	}
-
-	return 8, nil
-}
-
-// ReadFrom reads a UUID from the provided io.Reader.
-//
-// Encodes:
-//   - A UUID.
-//
-// Notes:
-//   - Encoded as an unsigned 128-bit integer (or two unsigned 64-bit integers: the most significant 64 bits and then the least significant 64 bits).
-func (U *UUID) ReadFrom(r io.Reader) (n int64, err error) {
-	nBytes, err := io.ReadFull(r, (*U)[:])
-
-	return int64(nBytes), err
-}
-
-// WriteTo writes a UUID to the provided io.Writer.
-func (U UUID) WriteTo(w io.Writer) (n int64, err error) {
-	nBytes, err := w.Write(U[:])
-
-	return int64(nBytes), err
-}
-
-// ReadFrom reads an UnsignedShort from the provided io.Reader.
-//
-// Encodes:
-//   - An integer between 0 and 65535.
-//
-// Notes:
-//   - Unsigned 16-bit integer.
-func (u *UnsignedShort) ReadFrom(r io.Reader) (n int64, err error) {
-	var buf [2]byte
-
-	if _, err = io.ReadFull(r, buf[:]); err != nil {
-		return 0, err
-	}
-	*u = UnsignedShort(buf[0])<<8 | UnsignedShort(buf[1])
-	n = int64(len(buf))
-
-	return n, nil
-}
-
-// WriteTo writes an UnsignedShort to the provided io.Writer.
-func (u UnsignedShort) WriteTo(w io.Writer) (n int64, err error) {
-	var buf [2]byte
-
-	buf[0] = byte(u >> 8)
-	buf[1] = byte(u & 0xFF)
-	if _, err = w.Write(buf[:]); err != nil {
-		return 0, err
-	}
-
-	return int64(len(buf)), nil
-}
-
-// ReadFrom reads a String from the provided io.Reader.
-//
-// Encodes:
-//   - A sequence of Unicode scalar values.
-//
-// Notes:
-//   - UTF-8 string prefixed with its size in bytes as a VarInt. Maximum length of n characters, which varies by context; https://minecraft.wiki/w/Java_Edition_protocol/Data_types#Type:String.
-func (s *String) ReadFrom(r io.Reader) (n int64, err error) {
-	var length VarInt
-	var nLen int64
-
-	if nLen, err = length.ReadFrom(r); err != nil {
-		return 0, err
-	}
-	buf := make([]byte, length)
-	var nStr int
-	if nStr, err = io.ReadFull(r, buf); err != nil {
-		return nLen + int64(nStr), err
-	}
-	*s = String(buf)
-
-	return nLen + int64(nStr), nil
-}
-
-// WriteTo writes a String to the provided io.Writer.
-func (s String) WriteTo(w io.Writer) (n int64, err error) {
-	strBytes := []byte(s)
-	length := VarInt(len(strBytes))
-
-	if n, err = length.WriteTo(w); err != nil {
-		return
-	}
-	var nStr int
-	if nStr, err = w.Write(strBytes); err != nil {
-		return n + int64(nStr), err
-	}
-
-	return n + int64(nStr), nil
-}
-
-// ReadFrom read a VarInt from the provided io.Reader.
-//
-// Encodes:
-//   - An integer between -2147483648 and 2147483647.
-//
-// Notes:
-//   - Variable-length data encoding a two's complement signed 32-bit integer; https://minecraft.wiki/w/Java_Edition_protocol/Data_types#VarInt_and_VarLong.
-func (v *VarInt) ReadFrom(r io.Reader) (n int64, err error) {
-	var position uint
-
-	for i := 0; i < 5; i++ {
-		var b [1]byte
-		var read int
-
-		if read, err = io.ReadFull(r, b[:]); err != nil {
-			return n, err
-		}
-		n += int64(read)
-		*v |= VarInt(b[0]&0x7F) << position
-		if b[0]&0x80 == 0 {
-			return
-		}
-		position += 7
-	}
-
-	return n, fmt.Errorf("VarInt trop long")
-}
-
-// WriteTo writes a VarInt to the provided io.Writer.
-func (v VarInt) WriteTo(w io.Writer) (n int64, err error) {
-	for {
-		temp := byte(v & 0x7F)
-
-		v >>= 7
-		if v != 0 {
-			temp |= 0x80
-		}
-		var written int
-		if written, err = w.Write([]byte{temp}); err != nil {
-			return
-		}
-		n += int64(written)
-		if v == 0 {
-			break
-		}
-	}
-
-	return
-}
-
-func (v VarInt) Len() int {
-	val := uint32((v << 1) ^ (v >> 31))
-	n := 1
-
-	for val >= 0x80 {
-		val >>= 7
-		n++
-	}
-
-	return n
-}
-
 func (b *Boolean) ReadFrom(r io.Reader) (n int64, err error) {
 	var buf [1]byte
-
 	if _, err = io.ReadFull(r, buf[:]); err != nil {
 		return 0, fmt.Errorf("error reading Boolean: %w", err)
 	}
@@ -212,126 +21,344 @@ func (b *Boolean) ReadFrom(r io.Reader) (n int64, err error) {
 	default:
 		return 1, fmt.Errorf("invalid value for Boolean: %x", buf[0])
 	}
-
 	return 1, nil
 }
 
 func (b *Boolean) WriteTo(w io.Writer) (n int64, err error) {
 	var buf [1]byte
-
 	if *b {
 		buf[0] = 0x01
 	} else {
 		buf[0] = 0x00
 	}
-	written, err := w.Write(buf[:])
-	if err != nil {
-		return int64(written), fmt.Errorf("error writing Boolean: %w", err)
+	if _, err = w.Write(buf[:]); err != nil {
+		return 0, fmt.Errorf("error writing Boolean: %w", err)
 	}
-
-	return int64(written), nil
+	return 1, nil
 }
 
-func (p *PArray[E]) ReadFrom(r io.Reader) (n int64, err error) {
-	var length VarInt
+func (b *Byte) ReadFrom(r io.Reader) (n int64, err error) {
+	var buf [1]byte
+	if _, err = io.ReadFull(r, buf[:]); err != nil {
+		return 0, fmt.Errorf("error reading Byte: %w", err)
+	}
+	*b = Byte(int8(buf[0]))
+	return 1, nil
+}
 
-	nn, err := length.ReadFrom(r)
+func (b *Byte) WriteTo(w io.Writer) (n int64, err error) {
+	var buf [1]byte
+	buf[0] = byte(*b)
+	if _, err = w.Write(buf[:]); err != nil {
+		return 0, fmt.Errorf("error writing Byte: %w", err)
+	}
+	return 1, nil
+}
+
+func (u *UnsignedByte) ReadFrom(r io.Reader) (n int64, err error) {
+	var buf [1]byte
+	if _, err = io.ReadFull(r, buf[:]); err != nil {
+		return 0, fmt.Errorf("error reading UnsignedByte: %w", err)
+	}
+	*u = UnsignedByte(buf[0])
+	return 1, nil
+}
+
+func (u *UnsignedByte) WriteTo(w io.Writer) (n int64, err error) {
+	var buf [1]byte
+	buf[0] = byte(*u)
+	if _, err = w.Write(buf[:]); err != nil {
+		return 0, fmt.Errorf("error writing UnsignedByte: %w", err)
+	}
+	return 1, nil
+}
+
+func (s *Short) ReadFrom(r io.Reader) (n int64, err error) {
+	var buf [2]byte
+	if _, err = io.ReadFull(r, buf[:]); err != nil {
+		return 0, fmt.Errorf("error reading Short: %w", err)
+	}
+	*s = Short(binary.BigEndian.Uint16(buf[:]))
+	return 2, nil
+}
+
+func (s *Short) WriteTo(w io.Writer) (n int64, err error) {
+	var buf [2]byte
+	binary.BigEndian.PutUint16(buf[:], uint16(*s))
+	if _, err = w.Write(buf[:]); err != nil {
+		return 0, fmt.Errorf("error writing Short: %w", err)
+	}
+	return 2, nil
+}
+
+func (u *UnsignedShort) ReadFrom(r io.Reader) (n int64, err error) {
+	var buf [2]byte
+	if _, err = io.ReadFull(r, buf[:]); err != nil {
+		return 0, fmt.Errorf("error reading UnsignedShort: %w", err)
+	}
+	*u = UnsignedShort(binary.BigEndian.Uint16(buf[:]))
+	return 2, nil
+}
+
+func (u *UnsignedShort) WriteTo(w io.Writer) (n int64, err error) {
+	var buf [2]byte
+	binary.BigEndian.PutUint16(buf[:], uint16(*u))
+	if _, err = w.Write(buf[:]); err != nil {
+		return 0, fmt.Errorf("error writing UnsignedShort: %w", err)
+	}
+	return 2, nil
+}
+
+func (i *Int) ReadFrom(r io.Reader) (n int64, err error) {
+	var buf [4]byte
+	if _, err = io.ReadFull(r, buf[:]); err != nil {
+		return 0, fmt.Errorf("error reading Int: %w", err)
+	}
+	*i = Int(binary.BigEndian.Uint32(buf[:]))
+	return 4, nil
+}
+
+func (i *Int) WriteTo(w io.Writer) (n int64, err error) {
+	var buf [4]byte
+	binary.BigEndian.PutUint32(buf[:], uint32(*i))
+	if _, err = w.Write(buf[:]); err != nil {
+		return 0, fmt.Errorf("error writing Int: %w", err)
+	}
+	return 4, nil
+}
+
+func (l *Long) ReadFrom(r io.Reader) (n int64, err error) {
+	var buf [8]byte
+	if _, err = io.ReadFull(r, buf[:]); err != nil {
+		return 0, fmt.Errorf("error reading Long: %w", err)
+	}
+	*l = Long(binary.BigEndian.Uint64(buf[:]))
+	return 8, nil
+}
+
+func (l *Long) WriteTo(w io.Writer) (n int64, err error) {
+	var buf [8]byte
+	binary.BigEndian.PutUint64(buf[:], uint64(*l))
+	if _, err = w.Write(buf[:]); err != nil {
+		return 0, fmt.Errorf("error writing Long: %w", err)
+	}
+	return 8, nil
+}
+
+func (f *Float) ReadFrom(r io.Reader) (n int64, err error) {
+	var buf [4]byte
+	if _, err = io.ReadFull(r, buf[:]); err != nil {
+		return 0, fmt.Errorf("error reading Float: %w", err)
+	}
+	*f = Float(math.Float32frombits(binary.BigEndian.Uint32(buf[:])))
+	return 4, nil
+}
+
+func (f *Float) WriteTo(w io.Writer) (n int64, err error) {
+	var buf [4]byte
+	binary.BigEndian.PutUint32(buf[:], math.Float32bits(float32(*f)))
+	if _, err = w.Write(buf[:]); err != nil {
+		return 0, fmt.Errorf("error writing Float: %w", err)
+	}
+
+	return 4, nil
+}
+
+func (d *Double) ReadFrom(r io.Reader) (n int64, err error) {
+	var buf [8]byte
+	if _, err = io.ReadFull(r, buf[:]); err != nil {
+		return 0, fmt.Errorf("error reading Double: %w", err)
+	}
+
+	*d = Double(math.Float64frombits(binary.BigEndian.Uint64(buf[:])))
+	return 8, nil
+}
+
+func (d *Double) WriteTo(w io.Writer) (n int64, err error) {
+	var buf [8]byte
+	binary.BigEndian.PutUint64(buf[:], math.Float64bits(float64(*d)))
+	if _, err = w.Write(buf[:]); err != nil {
+		return 0, fmt.Errorf("error writing Double: %w", err)
+	}
+	return 8, nil
+}
+
+func (s *String) ReadFrom(r io.Reader) (n int64, err error) {
+	var length VarInt
+	nLen, err := length.ReadFrom(r)
 	if err != nil {
-		return n, fmt.Errorf("error reading prefixed array length: %w", err)
+		return 0, fmt.Errorf("error reading String length: %w", err)
+	}
+	buf := make([]byte, int(length))
+	nStr, err := io.ReadFull(r, buf)
+	if err != nil {
+		return nLen + int64(nStr), fmt.Errorf("error reading String: %w", err)
+	}
+	*s = String(buf)
+	return nLen + int64(nStr), nil
+}
+
+func (s *String) WriteTo(w io.Writer) (n int64, err error) {
+	length := VarInt(len(*s))
+	n, err = length.WriteTo(w)
+	if err != nil {
+		return n, fmt.Errorf("error writing String length: %w", err)
+	}
+	nStr, err := w.Write([]byte(*s))
+	if err != nil {
+		return n + int64(nStr), fmt.Errorf("error writing String: %w", err)
+	}
+	return n + int64(nStr), nil
+}
+
+func (v *VarInt) ReadFrom(r io.Reader) (n int64, err error) {
+	var position uint
+	*v = 0
+	for i := 0; i < 5; i++ {
+		var b [1]byte
+		var read int
+
+		if read, err = io.ReadFull(r, b[:]); err != nil {
+			return n, fmt.Errorf("error reading VarInt: %w", err)
+		}
+		n += int64(read)
+		*v |= VarInt(b[0]&0x7F) << position
+		if b[0]&0x80 == 0 {
+			return n, nil
+		}
+		position += 7
+	}
+	return n, fmt.Errorf("VarInt too long")
+}
+
+func (v *VarInt) WriteTo(w io.Writer) (n int64, err error) {
+	val := *v
+	for {
+		temp := byte(val & 0x7F)
+		val >>= 7
+		if val != 0 {
+			temp |= 0x80
+		}
+		var written int
+		if written, err = w.Write([]byte{temp}); err != nil {
+			return n, fmt.Errorf("error writing VarInt: %w", err)
+		}
+		n += int64(written)
+		if val == 0 {
+			break
+		}
+	}
+	return n, nil
+}
+
+func (U *UUID) ReadFrom(r io.Reader) (n int64, err error) {
+	nBytes, err := io.ReadFull(r, (*U)[:])
+	if err != nil {
+		return int64(nBytes), fmt.Errorf("error reading UUID: %w", err)
+	}
+	return int64(nBytes), nil
+}
+
+func (U *UUID) WriteTo(w io.Writer) (n int64, err error) {
+	nBytes, err := w.Write(U[:])
+	if err != nil {
+		return int64(nBytes), fmt.Errorf("error writing UUID: %w", err)
+	}
+	return int64(nBytes), nil
+}
+
+func (P *PrefixedOptional[X]) ReadFrom(r io.Reader) (n int64, err error) {
+	nn, err := P.Has.ReadFrom(r)
+	if err != nil {
+		return n, fmt.Errorf("error reading PrefixedOptional flag: %w", err)
 	}
 	n += nn
+	if P.Has {
+		if fieldInstance, ok := any(P.Value).(Field); ok {
+			nn, err := fieldInstance.ReadFrom(r)
+			if err != nil {
+				return n, fmt.Errorf("error reading PrefixedOptional value: %w", err)
+			}
+			n += nn
+		} else {
+			typeName := reflect.TypeOf(new(X)).Elem().String()
+			return n, fmt.Errorf("type %s does not implement mc.Field, cannot read optional value", typeName)
+		}
+	} else {
+		P.Value = nil
+	}
+	return n, nil
+}
 
+func (P *PrefixedOptional[X]) WriteTo(w io.Writer) (n int64, err error) {
+	nn, err := P.Has.WriteTo(w)
+	if err != nil {
+		return n, fmt.Errorf("error writing PrefixedOptional flag: %w", err)
+	}
+	n += nn
+	if P.Has {
+		if fieldInstance, ok := any(P.Value).(Field); ok {
+			nn, err := fieldInstance.WriteTo(w)
+			if err != nil {
+				return n, fmt.Errorf("error writing PrefixedOptional value: %w", err)
+			}
+			n += nn
+		} else {
+			typeName := reflect.TypeOf(new(X)).Elem().String()
+			return n, fmt.Errorf("type %s does not implement mc.Field, cannot write optional value", typeName)
+		}
+	}
+	return n, nil
+}
+
+func (p *PrefixedArray[X]) ReadFrom(r io.Reader) (n int64, err error) {
+	var length VarInt
+	nn, err := length.ReadFrom(r)
+	if err != nil {
+		return n, fmt.Errorf("error reading PrefixedArray length: %w", err)
+	}
+	n += nn
 	if cap(*p.Slice) < int(length) {
-		*p.Slice = make([]E, length)
+		*p.Slice = make([]X, length)
 	} else {
 		*p.Slice = (*p.Slice)[:length]
 	}
-
 	for i := 0; i < int(length); i++ {
 		elemAddr := &(*p.Slice)[i]
-
 		fieldInstance, ok := any(elemAddr).(Field)
 		if !ok {
 			typeName := reflect.TypeOf(elemAddr).String()
 			return n, fmt.Errorf("element of type %s does not implement mc.Field required for reading", typeName)
 		}
-
 		nn, err := fieldInstance.ReadFrom(r)
 		if err != nil {
-			return n, fmt.Errorf("error reading element %d of prefixed array: %w", i, err)
+			return n, fmt.Errorf("error reading element %d of PrefixedArray: %w", i, err)
 		}
 		n += nn
 	}
-
 	return n, nil
 }
 
-func (p *PArray[E]) WriteTo(w io.Writer) (n int64, err error) {
+func (p *PrefixedArray[X]) WriteTo(w io.Writer) (n int64, err error) {
 	currentSlice := *p.Slice
 	length := VarInt(len(currentSlice))
-
 	nn, err := length.WriteTo(w)
 	if err != nil {
-		return n, fmt.Errorf("error writing prefixed array length: %w", err)
+		return n, fmt.Errorf("error writing PrefixedArray length: %w", err)
 	}
 	n += nn
-
 	for i := range currentSlice {
 		elemAddr := &currentSlice[i]
-
 		fieldInstance, ok := any(elemAddr).(Field)
 		if !ok {
 			typeName := reflect.TypeOf(elemAddr).String()
 			return n, fmt.Errorf("element of type %s does not implement mc.Field required for writing", typeName)
 		}
-
 		nn, err := fieldInstance.WriteTo(w)
 		if err != nil {
-			return n, fmt.Errorf("error writing element %d of prefixed array: %w", i, err)
+			return n, fmt.Errorf("error writing element %d of PrefixedArray: %w", i, err)
 		}
 		n += nn
 	}
-
-	return n, nil
-}
-
-func (P *POptional[E]) ReadFrom(r io.Reader) (n int64, err error) {
-	if _, err := P.Has.ReadFrom(r); err != nil {
-		return n, err
-	}
-	n += 1
-
-	if P.Has {
-		if fieldInstance, ok := any(&P.Value).(Field); ok {
-			nn, err := fieldInstance.ReadFrom(r)
-			if err != nil {
-				return n, err
-			}
-			n += nn
-		} else {
-			return n, nil
-		}
-	}
-
-	return n, nil
-}
-
-func (P *POptional[E]) WriteTo(w io.Writer) (n int64, err error) {
-	if nn, err := P.Has.WriteTo(w); err != nil {
-		return n, err
-	} else {
-		n += nn
-	}
-
-	if P.Has {
-		if fieldInstance, ok := any(&P.Value).(Field); ok {
-			if nn, err := fieldInstance.WriteTo(w); err != nil {
-				return n, err
-			} else {
-				n += nn
-			}
-		}
-	}
-
 	return n, nil
 }
--- internal/mc/types.go
+++ internal/mc/types.go
@@ -12,23 +12,138 @@ type Field interface {
 }
 
 type (
-	VarInt        int32
-	String        string
+	// Boolean Encodes:
+	//  - Either false or true.
+	// Size:
+	//  - 1 byte
+	// Notes:
+	//  - True is encoded as 0x01, false as 0x00.
+	Boolean bool
+	// Byte Encodes:
+	//  - An integer between -128 and 127.
+	// Size:
+	//  - 1 byte
+	// Notes:
+	//  - Signed 8-bit integer, two's complement.
+	Byte int8
+	// UnsignedByte Encodes:
+	//  - An integer between 0 and 255.
+	// Size:
+	//  - 1 byte
+	// Notes:
+	//  - Unsigned 8-bit integer.
+	UnsignedByte byte
+	// Short Encodes:
+	//  - An integer between -32768 and 32767.
+	// Size:
+	//  - 2 bytes
+	// Notes:
+	//  - Signed 16-bit integer, two's complement.
+	Short int16
+	// UnsignedShort Encodes:
+	//  - An integer between 0 and 65535.
+	// Size:
+	//  - 2 bytes
+	// Notes:
+	//  - Unsigned 16-bit integer
 	UnsignedShort uint16
-	UUID          uuid.UUID
-	Long          int64
-	Boolean       bool
-	// look if we can modify any with a list of mc types, to be less error-prone
-	PArray[E any] struct {
-		Slice *[]E
-	}
-	POptional[E any] struct {
+	// Int Encodes:
+	//  - An integer between -2147483648 and 2147483647.
+	// Size:
+	//  - 4 bytes
+	// Notes:
+	//  - Signed 32-bit integer, two's complement.
+	Int int32
+	// Long Encodes:
+	//  - An integer between -9223372036854775808 and 9223372036854775807.
+	// Size:
+	//  - 8 bytes
+	// Notes:
+	//  - Signed 64-bit integer, two's complement.
+	Long int64
+	// Float Encodes:
+	//  - A single-precision 32-bit IEEE 754 floating point number.
+	// Size:
+	//  - 4 bytes
+	Float float32
+	// Double Encodes:
+	//  - A double-precision 64-bit IEEE 754 floating point number.
+	// Size:
+	//  - 8 bytes
+	Double float64
+	// String (n) Encodes:
+	//  - A sequence of Unicode scalar values.
+	// Size:
+	//  - ≥ 1 ≤ (n×3) + 3 bytes
+	// Notes:
+	//  - UTF-8 string prefixed with its size in bytes as a VarInt. Maximum length of n characters, which varies by context; https://minecraft.wiki/w/Java_Edition_protocol/Data_types#Type:String.
+	String string
+	// VarInt (n) Encodes:
+	//  - An integer between -2147483648 and 2147483647.
+	// Size:
+	//  - ≥ 1 ≤ 5 bytes
+	// Notes:
+	//  - Variable-length data encoding a two's complement signed 32-bit integer; https://minecraft.wiki/w/Java_Edition_protocol/Data_types#VarInt_and_VarLong.
+	VarInt int32
+	// UUID Encodes:
+	//  - A UUID; https://minecraft.wiki/w/Java_Edition_protocol/Data_types#Type:UUID.
+	// Size:
+	//  - 16 bytes
+	// Notes:
+	//  - Encoded as an unsigned 128-bit integer (or two unsigned 64-bit integers: the most significant 64 bits and then the least significant 64 bits).
+	UUID uuid.UUID
+	// PrefixedOptional of X Encodes:
+	//  - A boolean and if present, a field of type X.
+	// Size:
+	//  - size of Boolean + (is present ? Size of X : 0) bytes
+	// Notes:
+	//  - The boolean is true if the field is present.
+	// todo: look if we can modify any with a list of mc types, to be less error-prone
+	PrefixedOptional[X any] struct {
 		Has   Boolean
-		Value E
+		Value *X
+	}
+	// PrefixedArray of X Encodes:
+	//  - https://minecraft.wiki/w/Java_Edition_protocol/Data_types#Prefixed_Array.
+	// Size:
+	//  - size of VarInt + size of X * length bytes
+	// Notes:
+	//  - A length-prefixed array.
+	// todo: same as above
+	PrefixedArray[X any] struct {
+		Slice *[]X
 	}
 )
 
-// Following types use auto-generated code to implement Field interface.
+func (v *VarInt) Len() int {
+	val := uint32((*v << 1) ^ (*v >> 31))
+	n := 1
+	for val >= 0x80 {
+		val >>= 7
+		n++
+	}
+	return n
+}
+
+func NewPrefixedArray[E any](slice *[]E) *PrefixedArray[E] {
+	return &PrefixedArray[E]{
+		Slice: slice,
+	}
+}
+
+func NewPrefixedOptional[E any](value *E) *PrefixedOptional[E] {
+	return &PrefixedOptional[E]{
+		Has:   value != nil,
+		Value: value,
+	}
+}
+
+func (P *PrefixedOptional[X]) Set(value *X) {
+	P.Value = value
+	P.Has = value != nil
+}
+
+// Following types use auto-generated code to implement the Field interface.
 
 //go:generate-field-impl
 type DataPackIdentifier struct {
@@ -41,25 +156,11 @@ type DataPackIdentifier struct {
 type RegistryDataEntry struct {
 	ID String
 	// todo: this is supposed to be an optional NTB, change later
-	Data POptional[*types.Nil]
+	Data PrefixedOptional[types.Nil]
 }
 
 //go:generate-field-impl
 type RegistryData struct {
 	ID      String
-	Entries PArray[RegistryDataEntry]
-}
-
-func NewPArray[E any](slice *[]E) *PArray[E] {
-	return &PArray[E]{
-		Slice: slice,
-	}
-}
-
-// todo: like PArray, Value should be a pointer to avoid copying large structs, memory issues and potential errors
-func NewPOptional[E any](value E) *POptional[E] {
-	return &POptional[E]{
-		Has:   false,
-		Value: value,
-	}
+	Entries PrefixedArray[RegistryDataEntry]
 }
